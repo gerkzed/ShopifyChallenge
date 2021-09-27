81e09672474cb4ef0dae205a14d03a93f1622550
gerkzed <robbiecampbell89@gmail.com>
2021-09-27 19:50:40 -0400

    Add project files.

---
+++ ShopifyChallenge/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace ShopifyChallenge
+{
+    partial class mainfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addPhotoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPhotoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListView1 = new System.Windows.Forms.ListView();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.delphoto = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewFullsizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addPhotoToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(132, 26);
+            // 
+            // addPhotoToolStripMenuItem
+            // 
+            this.addPhotoToolStripMenuItem.Name = "addPhotoToolStripMenuItem";
+            this.addPhotoToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.addPhotoToolStripMenuItem.Text = "Add Photo";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addPhotoToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // addPhotoToolStripMenuItem1
+            // 
+            this.addPhotoToolStripMenuItem1.Name = "addPhotoToolStripMenuItem1";
+            this.addPhotoToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
+            this.addPhotoToolStripMenuItem1.Text = "Add Photo";
+            this.addPhotoToolStripMenuItem1.Click += new System.EventHandler(this.addPhotoToolStripMenuItem1_Click);
+            // 
+            // ListView1
+            // 
+            this.ListView1.HideSelection = false;
+            this.ListView1.Location = new System.Drawing.Point(45, 27);
+            this.ListView1.Name = "ListView1";
+            this.ListView1.Size = new System.Drawing.Size(289, 209);
+            this.ListView1.TabIndex = 2;
+            this.ListView1.UseCompatibleStateImageBehavior = false;
+            this.ListView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListView1_MouseClick);
+            this.ListView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListView1_MouseDown);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(25, 270);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(322, 269);
+            this.PictureBox1.TabIndex = 3;
+            this.PictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.delphoto,
+            this.viewFullsizeToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(143, 48);
+            // 
+            // delphoto
+            // 
+            this.delphoto.Name = "delphoto";
+            this.delphoto.Size = new System.Drawing.Size(142, 22);
+            this.delphoto.Text = "Delete Photo";
+            this.delphoto.Click += new System.EventHandler(this.deletePhotoToolStripMenuItem_Click);
+            // 
+            // viewFullsizeToolStripMenuItem
+            // 
+            this.viewFullsizeToolStripMenuItem.Name = "viewFullsizeToolStripMenuItem";
+            this.viewFullsizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewFullsizeToolStripMenuItem.Text = "View Fullsize";
+            this.viewFullsizeToolStripMenuItem.Click += new System.EventHandler(this.viewFullsizeToolStripMenuItem_Click);
+            // 
+            // mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(381, 583);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.ListView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainfrm";
+            this.Text = "Photo Repository";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addPhotoToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPhotoToolStripMenuItem1;
+        private System.Windows.Forms.ListView ListView1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem delphoto;
+        private System.Windows.Forms.ToolStripMenuItem viewFullsizeToolStripMenuItem;
+    }
+}
+
---
+++ ShopifyChallenge/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ShopifyChallenge
+{
+
+    public partial class mainfrm : Form
+
+    {
+
+
+        
+        public string datadir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"PhotoRepository\");
+        
+        
+        public Image image;
+        public mainfrm()
+        {
+
+                string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                Directory.CreateDirectory(datadir);
+                Directory.CreateDirectory(datadir + "pictures");
+                
+
+                if (File.Exists(datadir + "pics.txt"))
+                {
+
+                }
+
+                else
+                {
+
+                    // Create a new file     
+                    using (FileStream fs = File.Create(datadir + "pics.txt"))
+                    {
+                        Console.WriteLine("textfile created");
+                    }
+
+                }
+            
+
+
+            InitializeComponent();
+
+            BuildListView();
+            FillListView();
+            InitializePictureBox();
+            
+        }
+        private void BuildListView()
+        {
+
+            string[] pics2 = RefreshData();
+
+            if (pics2.Length == 0)
+            {
+                if (MessageBox.Show("No photo's detected, click OK to begin adding", "Add Photo", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                    FirstPhoto();
+                }
+            }
+                
+                
+
+                ColumnHeader header1, header2;
+                header1 = new ColumnHeader();
+                header2 = new ColumnHeader();
+
+                header1.Text = "File name";
+                header1.TextAlign = HorizontalAlignment.Left;
+                header1.Width = -1;
+
+                header2.TextAlign = HorizontalAlignment.Left;
+                header2.Text = "Location";
+                header2.Width = -1;
+
+                ListView1.Columns.Add(header1);
+                ListView1.Columns.Add(header2);
+                ListView1.View = View.Details;
+            }
+        
+        private string [] RefreshData()
+        {
+            string[] pics1 = File.ReadAllLines(datadir + "pics.txt");
+            return pics1;
+        }
+        private void FillListView()
+        {
+            string[] pics2 = RefreshData();
+
+            if (pics2 != null)
+            {
+                foreach (string pic in pics2)
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(pic));
+                    item.SubItems.Add(pic);
+                    ListView1.Items.Add(item);
+                }
+            }
+        }
+
+        private void InitializePictureBox()
+        {
+           
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            this.Controls.Add(this.PictureBox1);
+        }
+
+        private void MoveImage(string dir, string name)
+        {
+
+            File.Copy(dir, datadir + @"pictures\" + name, true);
+
+        }
+
+        private void ListView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (image != null)
+            {
+                PictureBox1.Image = null;
+                image.Dispose();
+            }
+
+            try
+            {
+                ListViewItem selection = ListView1.GetItemAt(e.X, e.Y);
+                if (selection != null)
+                {
+                    image = Image.FromFile(selection.SubItems[1].Text);
+
+                    PictureBox1.Image = image;
+
+                }
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+
+
+            }
+        }
+
+        private void ListView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Right)
+            {
+                ListViewItem item = ListView1.GetItemAt(e.X, e.Y);
+                if (item != null)
+                {
+                    item.Selected = true;
+                    contextMenuStrip2.Show(ListView1, e.Location);
+                }
+            }
+        }
+
+
+        private void FirstPhoto()
+        {
+
+            var dup = false;
+            var pics1 = RefreshData();
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select An Image(s)";
+            openDialog.Multiselect = true;
+            openDialog.Filter = "Image Files (*.BMP;*.DIB;*.RLE;*.JPG;*.JPEG;*.JPE;*.JFIF;*.GIF;*.TIF;*.TIFF;*.PNG)|*.BMP;*.DIB;*.RLE;*.JPG;*.JPEG;*.JPE;*.JFIF;*.GIF;*.TIF;*.TIFF;*.PNG";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                for (int i = 0; i < pics1.Length; i++)
+                {
+                    if (datadir + openDialog.SafeFileName == pics1[i])
+                    {
+                        MessageBox.Show("This image is already in your repository.", "Duplicate Photo", MessageBoxButtons.OK);
+                        dup = true;
+                        break;
+                    }
+
+
+                }
+                if (dup == false)
+                {
+                    string[] files = openDialog.FileNames;
+                    string[] safenames = openDialog.SafeFileNames;
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        StreamWriter sw = new StreamWriter(datadir + "pics.txt", true);
+
+                        sw.WriteLine(datadir + @"pictures\" + safenames[i]);
+
+                        MoveImage(files[i], safenames[i]);
+
+                        sw.Close();
+                    }
+                }
+
+            }
+        }
+
+        private void addPhotoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var pics1 = RefreshData();
+            var dup = false;
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Multiselect = true;
+            openDialog.Title = "Select An Image(s)";
+            openDialog.Filter = "Image Files (*.BMP;*.DIB;*.RLE;*.JPG;*.JPEG;*.JPE;*.JFIF;*.GIF;*.TIF;*.TIFF;*.PNG)|*.BMP;*.DIB;*.RLE;*.JPG;*.JPEG;*.JPE;*.JFIF;*.GIF;*.TIF;*.TIFF;*.PNG";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                for (int i = 0; i < pics1.Length; i++)
+                {
+                    if (datadir + openDialog.SafeFileName == pics1[i])
+                    {
+                        MessageBox.Show("This image is already in your repository.", "Duplicate Photo", MessageBoxButtons.OK);
+                        dup = true;
+                        break;
+                    }
+
+
+                }
+
+                if (dup == false)
+                {
+                    string[] files = openDialog.FileNames;
+                    string[] safenames = openDialog.SafeFileNames;
+
+                    for(int i = 0; i < files.Length; i++)
+                    {
+                        StreamWriter sw = new StreamWriter(datadir + "pics.txt", true);
+
+                        sw.WriteLine(datadir + @"pictures\" + safenames[i]);
+
+                        MoveImage(files[i], safenames[i]);
+
+                        sw.Close();
+
+                        ListView1.Items.Clear();
+                        FillListView();
+                        pics1 = RefreshData();
+                    }
+                }
+                
+
+            }
+        }
+
+        private void deletePhotoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            string[] pics3 = RefreshData();
+            if (MessageBox.Show("Are you sure you want to delete " + ListView1.FocusedItem.Text, "Confirm Delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+
+                image.Dispose();
+
+                for (int i = 0; i < pics3.Length; i++)
+                {
+
+                    if (ListView1.FocusedItem.SubItems[1].Text == pics3[i])
+                    {
+
+                        List<string> linesList = File.ReadAllLines(datadir + "pics.txt").ToList();
+                        linesList.RemoveAt(i);
+                        File.WriteAllLines((datadir + "pics.txt"), linesList.ToArray());
+
+                        ListView1.Items.Clear();
+                        PictureBox1.Image = null;
+                        FillListView();
+
+                        if (File.Exists(pics3[i]))
+                        {
+
+                            try
+                            {
+                                PictureBox1.Image = null;
+                                image.Dispose();
+                                File.Delete(pics3[i]);
+                            }
+                            catch (IOException ea)
+                            {
+
+                                Console.WriteLine(ea.Message);
+                                return;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        private void viewFullsizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.AutoSize = true;
+            form.MinimumSize = new Size(image.Width, image.Height);
+            form.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Image = image;
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            form.Controls.Add(pictureBox);
+
+            form.ShowDialog();
+        }
+    }
+}
